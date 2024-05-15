7fc5ce05b87ae607f5340eb210ca133cb73115cd
laura-quilt <laura@quiltdata.io>
2024-05-15 11:37:20 -0400

    reorg demo data & nb updates

--- 2019-02-25_download_sra.sh
+++
@@ -1,8 +0,0 @@
-cd ~/quilt_data/20240115_ccle_metadata_schemas/fastq/2019-02-25/
-for SRA in $(cat 2019-02-25_sra.txt):
-do
-    echo $SRA
-    prefetch -v $SRA
-    fastq-dump --split-3 $SRA
-    rm -rf $SRA
-done;
